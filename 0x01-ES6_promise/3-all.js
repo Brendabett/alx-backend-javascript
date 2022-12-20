672f11abc6df088ff61ac69780a9bec03aade349
Brendabett <brendabett33@gmai>
2022-12-20 15:12:22 +0000

    My commit

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log();
+    }).catch(() => console.log('Signup system offline'));
+}
