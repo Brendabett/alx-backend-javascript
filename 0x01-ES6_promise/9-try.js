c8f7779aae9ec09ba46902eabca2288c941ca82e
Brendabett <brendabett33@gmai>
2022-12-20 15:46:23 +0000

    Await / Async

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,9 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction(), 'Guardrail was processed');
+  } catch (err) {
+    queue.push(, 'Guardrail was processed');
+  }
+  return queue;
+}
