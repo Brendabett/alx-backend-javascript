d6cf348149d361120d9de545cf74065a363b21f6
Brendabett <brendabett33@gmail.com>
2023-01-04 09:52:44 -0800

    My commit

---
+++ 0x04-TypeScript/webpack.config.js
@@ -0,0 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = {
